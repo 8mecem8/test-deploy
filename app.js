42f9fb8319b2fbca44b12c9a4e8bb895ef08484d
Cem  Onal <mehmetcemonal@gmail.com>
2023-03-07 13:11:56 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+//Import Node.js Packages...
+const express = require('express')
+
+
+//Import local Files...
+const middleware = require('./utilities/middleware.js');
+const { allRoutes } = require('./Routes/allRoutes');
+const { allMiddlewares } = require('./Middlewares/allMiddlewares');
+
+//Decleration Variables
+const app = express()
+
+
+
+// Middlewares-------------------------------------------------------------------------
+allMiddlewares(app)
+
+// Routes-------------------------------------------------------------------------
+allRoutes(app)
+
+
+//404 error middleware, **** must be at the end of all other routes
+//app.use(middleware.unknownEndpoint)
+
+module.exports = app
