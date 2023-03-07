5fc9a85fa552709c2e774f370ecca904f2d5191f
Cem  Onal <mehmetcemonal@gmail.com>
2023-03-07 13:15:44 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express')
 
 
 //Import local Files...
-const middleware = require('./utilities/middleware.js');
 const { allRoutes } = require('./Routes/allRoutes');
 const { allMiddlewares } = require('./Middlewares/allMiddlewares');
 
@@ -18,8 +17,5 @@ allMiddlewares(app)
 // Routes-------------------------------------------------------------------------
 allRoutes(app)
 
-
-//404 error middleware, **** must be at the end of all other routes
-//app.use(middleware.unknownEndpoint)
 
 module.exports = app
